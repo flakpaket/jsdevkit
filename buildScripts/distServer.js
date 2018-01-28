952ec735b932b21d2e771ea20f53a63e7ce85a3d
flakpaket <jon@Jons-MacBook-Pro.local>
2018-01-28 15:04:57 -0500

    Module 9 work in progress

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -3,18 +3,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(compression());
+app.use(express.static('dist'));
 
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+});
 
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
@@ -23,10 +23,6 @@ app.get('/users', function(req, res) {
     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
-});
-
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
 app.listen(port, function(err){
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,35 @@
 import path from 'path';
 import webpack from 'webpack';
+//import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/index'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
+
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
 
@@ -24,7 +39,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loaders: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
